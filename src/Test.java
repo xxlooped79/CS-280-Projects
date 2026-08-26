080f92ce4e864bffe7ebde47221c92499289f0d4
xxloooped79 <Ehernandez2@hcc-nd.edu>
2026-08-26 13:56:44 -0400

    commit all

--- src/Test.java
+++ src/Test.java
@@ -31,6 +31,7 @@ public class Test {
         /* Call additional main routines here as you create new sorting algorithms. */
 
         // Test data structures.
+        KeyValuePair.main(args);
         /* Call additional main routines as you create new data strutures. */
 
         return true;
