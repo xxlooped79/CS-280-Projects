be7f88545faebf5d81084ec80b3331fc321aa2b1
xxloooped79 <Ehernandez2@hcc-nd.edu>
2026-09-02 09:15:08 -0400

    commit Bubblesort.java

--- src/assignments/sorting/Bubblesort.java
+++ src/assignments/sorting/Bubblesort.java
@@ -1,66 +1,75 @@
 package assignments.sorting;
 
 /**
-* Swap Adjacent elemenets over and over until the whole array is sorted.
-*/
+ * Swap adjacent elements over and over until the whole array is sorted.
+ */
 
 public class Bubblesort extends SortingAlgorithm
 {
-
-    /** 
-     * sorting an array in-place using bubble sort 
-     * 
-     * Post-condition: 'array' is sorted in ascending order 
-     * 
-     * @param array an array of integers 
+    /**
+     * Sorting an array in-place using bubble sort.
+     *
+     * Post-condition: 'array' is sorted in ascending order.
+     *
+     * @param array an array of integers
      */
-    public void sort( Integer[] array){
-        for(int k= array.length; k >= 2; k--) {            //k is the length of the sub
-            for (int i=0; i < k-1; i ++) {
-                if (array[1] > array[i+1]) {
-                //swap adjcent items (i and i+1)
-                swap(array,i,i+1);
+    public void sort(Integer[] array)
+    {
+        for (int k = array.length; k >= 2; k--)
+        {
+            // k is the length of the unsorted section.
+            for (int i = 0; i < k - 1; i++)
+            {
+                // Compare adjacent elements.
+                if (array[i] > array[i + 1])
+                {
+                    // Swap adjacent items (i and i + 1).
+                    swap(array, i, i + 1);
                 }
             }
         }
     }
 
- /** 
-  * Swap two elements within an array 
-  * 
-  * @param array the array to swap values in
-  * @param i the first index to swap
-  * @param j the second index to swap
-  * 
-  */
-private void swap(Integer[] array, int i, int j) {
-    Integer temp = array[i];
-    array[i] = array[j];
-    array[j] = temp;
-    }
-public static void main(String[] args)
-{
-    SortingAlgorithm.validate(new Bubblesort());
-    System.out.println("Bubblesort has passed all tests");
-
-    // fill an array with random numbers
-
-    int N = 5;
-    Integer[] array = new Integer[N];
-
-    for(int i = 0; i < array.length; i++) {
-        array[i] = (int)(N*Math.random());
+    /**
+     * Swap two elements within an array.
+     *
+     * @param array the array to swap values in
+     * @param i the first index to swap
+     * @param j the second index to swap
+     */
+    private void swap(Integer[] array, int i, int j)
+    {
+        Integer temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
     }
 
-    //measuring runtime
+    public static void main(String[] args)
+    {
+        SortingAlgorithm.validate(new Bubblesort());
+        System.out.println("Bubblesort has passed all tests");
 
-    SortingAlgorithm sorter = new Bubblesort();
-    long start = System.nanoTime();
-    sorter.sort(array);
-    long end = System.nanoTime();
-    double duration = (end - start) / (1e9);
+        // Fill an array with random numbers.
+        int N = 5;
+        Integer[] array = new Integer[N];
 
-    System.out.println("Array size " +N);
-    System.out.println("Total duration: "+duration);
-}
+        for (int i = 0; i < array.length; i++)
+        {
+            array[i] = (int)(N * Math.random());
+        }
+
+        // Measuring runtime.
+        SortingAlgorithm sorter = new Bubblesort();
+
+        long start = System.nanoTime();
+
+        sorter.sort(array);
+
+        long end = System.nanoTime();
+
+        double duration = (end - start) / (1e9);
+
+        System.out.println("Array size " + N);
+        System.out.println("Total duration: " + duration);
+    }
 }
