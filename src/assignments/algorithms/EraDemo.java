b40eb19a2ffd35661edee84c3a64e52340bdaf4d
xxloooped79 <Ehernandez2@hcc-nd.edu>
2026-09-09 09:27:40 -0400

    Commit all

---
+++ src/assignments/algorithms/EraDemo.java
@@ -0,0 +1,39 @@
+
+package assignments.algorithms;
+
+import assignments.sorting.InsertionSort;
+
+public class EraDemo {
+
+    private static Integer[] randomArray(int n) {
+
+        Integer[] array = new Integer[n];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * n);
+        }
+
+        return array;
+    }
+
+    public static void main(String[] args) {
+        InsertionSort<Integer> sorter = new InsertionSort<>();
+        for (int N = 10; N < 100; N += 10) {
+
+            Integer[] array = randomArray(N);
+
+
+            long start = System.nanoTime();
+
+            sorter.sort(array);
+
+            long end = System.nanoTime();
+
+            double duration = (end - start) / 1e9;
+
+            System.out.println(N + "\t" + duration);
+        }
+    }
+}
+
+
