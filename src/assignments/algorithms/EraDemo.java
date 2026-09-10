a24ef9687a938b45950ae86edd8fe35c03d2c766
xxloooped79 <Ehernandez2@hcc-nd.edu>
2026-09-10 18:06:52 -0400

    Commit all

--- src/assignments/algorithms/EraDemo.java
+++ src/assignments/algorithms/EraDemo.java
@@ -1,48 +1,39 @@
 package assignments.algorithms;
+
+import assignments.sorting.InsertionSort;
+
 public class EraDemo {
 
+    private static Integer[] randomArray(int n) {
+
+        Integer[] array = new Integer[n];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * n);
+        }
+
+        return array;
+    }
+
     public static void main(String[] args) {
+        InsertionSort<Integer> sorter = new InsertionSort<>();
+        for (int N = 1000; N < 100000000; N *= 10) {
 
-        System.out.println("N\tDynamic\tBinet\tRecursive");
+            Integer[] array = randomArray(N);
 
-        for (int N = 1; N <= 40; N++) {
 
-            // Dynamic Programming
             long start = System.nanoTime();
 
-            DynamicFibonacci.fibonacci(N);
+            sorter.sort(array);
 
             long end = System.nanoTime();
 
-            double dynamicTime = (end - start) / 1e9;
+            double duration = (end - start) / 1e9;
 
-
-            // Binet's Formula
-            start = System.nanoTime();
-
-            BinetFibonacci.fibonacci(N);
-
-            end = System.nanoTime();
-
-            double binetTime = (end - start) / 1e9;
-
-
-            // Recursion
-            start = System.nanoTime();
-
-            RecursiveFibonacci.fibonacci(N);
-
-            end = System.nanoTime();
-
-            double recursiveTime = (end - start) / 1e9;
-
-
-            System.out.println(
-                    N + "\t" +
-                    dynamicTime + "\t" +
-                    binetTime + "\t" +
-                    recursiveTime
-            );
+            System.out.println(N + "\t" + duration);
         }
     }
 }
+
+
+
