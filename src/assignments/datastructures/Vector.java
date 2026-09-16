23dc0b4a9e8076554ba734ad54defe247219dc91
xxloooped79 <Ehernandez2@hcc-nd.edu>
2026-09-16 19:32:52 -0400

    Add Vector iterator

--- src/assignments/datastructures/Vector.java
+++ src/assignments/datastructures/Vector.java
@@ -1,6 +1,7 @@
 package assignments.datastructures;
 
 import adt.List;
+import java.util.Iterator;
 
 /// An extensible list backed by an array buffer.
 ///
@@ -10,10 +11,10 @@ import adt.List;
 ///
 /// Eventually, the array buffer *will* run out of space.
 /// Then you need to allocate an even larger buffer, and copy the present buffer to the new one.
-/// This is a very expensive operation, so you want to make sure it occurs very infrequently.
 ///
 /// @param <T> the type of each element
-public class Vector<T> implements List<T> {
+public class Vector<T> implements List<T>, Iterable<T> {
+
     /** The initial amount of buffer space in a newly-created vector. */
     public static final int INITIAL_BUFFER_SIZE = 10;
 
@@ -25,8 +26,6 @@ public class Vector<T> implements List<T> {
      */
     @SuppressWarnings("unchecked")
     public Vector() {
-        // Generic types don't technically exist at runtime,
-        // so allocate an Object array and cast it to T[].
         this.array = (T[])(new Object[INITIAL_BUFFER_SIZE]);
         this.size = 0;
     }
@@ -41,11 +40,10 @@ public class Vector<T> implements List<T> {
 
     /**
      * Fetch an item from the list.
-     * @param index the location of the item - a nonnegative integer less than the length of the list
+     * @param index the location of the item
      * @return the value stored at the given location
      */
     public T at(int index) {
-        // You may use assert statements to enforce pre-conditions at runtime.
         assert 0 <= index && index < this.size;
 
         return this.array[index];
@@ -53,11 +51,10 @@ public class Vector<T> implements List<T> {
 
     /**
      * Change an item in the list.
-     * @param index the location of the item - a nonnegative integer less than the length of the list
-     * @param value the new value to assign at the given location
+     * @param index the location of the item
+     * @param value the new value
      */
     public void set(int index, T value) {
-        // You may use assert statements to enforce pre-conditions at runtime.
         assert 0 <= index && index < this.size;
 
         this.array[index] = value;
@@ -84,48 +81,39 @@ public class Vector<T> implements List<T> {
 
     /**
      * Insert an item into the list.
-     * @param index the location of where to put the item - a nonnegative integer less than or equal to the length of the list
+     * @param index the location of where to put the item
      * @param value the new value to put at the given location
      */
     public void insert(int index, T value) {
-        // You may use assert statements to enforce pre-conditions at runtime.
-        // Note this function has a somewhat different pre-condition!
         assert 0 <= index && index <= this.size;
 
-        // If the array is full, double its size.
         if (this.size == this.array.length) {
             resize(this.array.length * 2);
         }
 
-        // Shift elements to the right to make room.
         for (int i = this.size; i > index; i--) {
             this.array[i] = this.array[i - 1];
         }
 
-        // Insert the new value.
         this.array[index] = value;
         this.size++;
     }
 
     /**
      * Remove an item from the list.
-     * @param index the location to delete from - a nonnegative integer less than the length of the list
+     * @param index the location to delete from
      * @return the value which was removed
      */
     public T delete(int index) {
-        // You may use assert statements to enforce pre-conditions at runtime.
         assert 0 <= index && index < this.size;
 
         T removed = this.array[index];
 
-        // Shift elements to the left.
         for (int i = index; i < this.size - 1; i++) {
             this.array[i] = this.array[i + 1];
         }
 
-        // Remove the duplicate reference at the end.
         this.array[this.size - 1] = null;
-
         this.size--;
 
         return removed;
@@ -134,15 +122,12 @@ public class Vector<T> implements List<T> {
     /**
      * Resize the internal buffer array.
      *
-     * This method involves copying from the current buffer to a newly allocated one.
-     *
      * @param newSize the new size of the internal buffer array
      */
     @SuppressWarnings("unchecked")
     private void resize(int newSize) {
         T[] newArray = (T[])(new Object[newSize]);
 
-        // Copy all currently-used elements.
         for (int i = 0; i < this.size; i++) {
             newArray[i] = this.array[i];
         }
@@ -151,11 +136,49 @@ public class Vector<T> implements List<T> {
     }
 
     /**
+     * Return an iterator that goes through the vector from beginning to end.
+     *
+     * @return an iterator for this vector
+     */
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            private int index = 0;
+
+            @Override
+            public boolean hasNext() {
+                return index < size;
+            }
+
+            @Override
+            public T next() {
+                return array[index++];
+            }
+        };
+    }
+
+    /**
      * Run validation tests.
      * @param args command-line args
      */
     public static void main(String[] args) {
         List.validate(new Vector<>());
+
+        // Test iterator.
+        Vector<Integer> vector = new Vector<>();
+
+        for (int i = 0; i < INITIAL_BUFFER_SIZE; i++) {
+            vector.insert(0, i);
+        }
+
+        Iterator<Integer> iter = vector.iterator();
+
+        for (int i = INITIAL_BUFFER_SIZE; i > 0; i--) {
+            assert iter.next().equals(i - 1);
+        }
+
+        assert !iter.hasNext();
+
         System.out.println("Vector passes all tests.");
     }
 }
